eba49e2b13c4ab7dbf6f514d28e77336d1a59221
Samantha Amoedo da Silva <samoedo.silva@gmail.com>
2022-06-25 21:37:20 -0300

    Jogos

--- JogodaVelha.php
+++ JogodaVelha.php
@@ -2,6 +2,7 @@
 
 $jogadorX = readline ("Digite o nome do jogador X:");
 $jogadorO = readline ("Digite o nome do jogador O:");
+  
 
 echo "\n
 |1| |2| |3|
@@ -21,21 +22,25 @@ $tabuleiro [8] = "|8|";
 $tabuleiro [9] = "|9|";
 
 $i = 0;
+$jogadas = 0;
 
 do{
-$posicaoX = readline ("Escolha uma posição Jogador ".$jogadorX.":");
-$posicaoO = readline ("Escolha uma posição Jogador ".$jogadorO.":");
-
+    $posicaoX = readline ("Escolha uma posição Jogador ".$jogadorX.":");
+    if($jogadas != 4){
+    $posicaoO = readline ("Escolha uma posição Jogador ".$jogadorO.":");
+    }
 $numeroX = $posicaoX;
 $numeroO = $posicaoO;
 
 if($posicaoX == $numeroX && $posicaoO == $numeroO){
     $tabuleiro[$numeroX] = "|X|";
     $tabuleiro[$numeroO] = "|O|";
+    $jogadas ++;
     echo"
     $tabuleiro[1] $tabuleiro[2] $tabuleiro[3]
     $tabuleiro[4] $tabuleiro[5] $tabuleiro[6]
     $tabuleiro[7] $tabuleiro[8] $tabuleiro[9]\n\n";
+    echo"jogadas $jogadas\n";
 }
 $i = $tabuleiro;
 if($i[1] == "|X|" && $i[2] == "|X|" && $i[3] == "|X|" || $i[1] == "|X|" && $i[4] == "|X|" && $i[7] == "|X|"){
@@ -68,5 +73,10 @@ elseif($i[1] == "|O|" && $i[5] == "|O|" && $i[9] == "|O|"){
     echo"Parabéns, o vencedor é o jogador $jogadorO!";
     exit();
 } 
+if($i[1] != "|1|" && $i[2] != "|2|" && $i[3] != "|3|" && $i[4] != "|4|" && $i[5] != "|5|" && $i[6] != "|6|" 
+&& $i[7] != "|7|" && $i[8] != "|8|" && $i[9] != "|9|"){
+    echo"Infelizmente ninguém ganhou o jogo :/";
+    exit();
+}
 }while($i != "|X|" || $i != "|O|");
 ?>
